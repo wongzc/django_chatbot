4c7649151a433be8d63124b5462925701ba8a680
wongzc <52870701+wongzc@users.noreply.github.com>
2023-09-12 10:04:46 +0800

    Initial commit

---
+++ chatbot/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import openai
+
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import Chat
+
+from django.utils import timezone
+# Create your views here.
+
+apikey='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'
+openai.api_key=apikey
+
+def ask_openai(message):
+    response =openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    print(response)
+
+    answer =response.choices[0].text.strip()
+    return answer
+
+def chatbot(request):
+    chats= Chat.objects.filter(user=request.user)
+    context={
+        'chats':chats
+    }
+    if request.method=='POST':
+        message=request.POST.get('message')
+        response=ask_openai(message)
+
+        chat=Chat(user=request.user, message=message, response=response, created_at=timezone.now)
+        chat.save()
+        return JsonResponse({'response':response,'message':message})
+    return render(request, 'chatbot.html',context)
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message='Invalid user!'
+            return render(request, 'login.html', {'error_message':error_message})
+    else:
+        return render(request, "login.html")
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        if password1==password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message='Error during create account!'
+                return render(request, 'register.html', {'error_message':error_message})
+        else:
+            error_message='Password not match!!'
+            return render(request, 'register.html', {'error_message':error_message})
+    return render(request, "register.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
